2da3d1f6a2fc965c7b6969547e44bad726f6e102
eugenegraves <elg22@njit.edu>
2024-03-05 21:46:26 -0500

    Feature #6 Finished

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import FilmPopup from "./FilmPopup";
 
-function Card({ id, title}) {
+function Card({ id, title, openPopup }) {
     return (
         <div className='card' key={id}>
             <h2 className='card-title'>{title}</h2>
             <div className='pseudo-picture'>
-                <button className='details-button' /*onClick={openPopup}*/>View Details</button>
+                <button className='details-button' onClick={openPopup}>View Details</button>
             </div>
         </div>
     );
 }
 
+function SpinnerOverlay() {
+    return (
+      <div className="spinner-overlay">
+        <div className="spinner-container">
+            <div className="spinner"></div>
+        </div>
+      </div>
+    );
+}
+
 const Search = () => {
+    const [filmDetails, setFilmDetails] = useState({});
     const [searchTerm, setSearchTerm] = useState('');
     const [filter, setFilter] = useState('Film'); //Sets the default filter to be by film.
     const [filteredData, setFilteredData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [popupActive, setPopupActive] = useState(false);
     const baseURL = "http://127.0.0.1:5000";
 
     const handleSearchChange = (event) => {
@@ -43,19 +57,38 @@ const Search = () => {
 
     const fetchData2 = async () => {
         try {
+            setIsLoading(true);
             await new Promise((resolve) => setTimeout(resolve, 3000));
+            setIsLoading(false);
             const url = `${baseURL}/film_search/${filter}?q=${searchTerm}`;
             const response = await axios.get(url);
             console.log(response.data);
             setFilteredData(response.data);
         } catch (error) {
             console.error(error);
+            setIsLoading(false);
           // Handle errors appropriately
         }
     };
 
+    const openPopup = async(title) => {
+        const baseURL = "http://127.0.0.1:5000";
+        console.log(title);
+        //const encodedTitle = encodeURIComponent(title);
+        const url = `${baseURL}/film_details?title=${title}`;
+        const response = await axios.get(url);
+        console.log(response.data);
+        setFilmDetails(response.data["0"]);
+        setPopupActive(true);
+    }
+
+    const closeFilmPopup = () => {
+        setPopupActive(false);
+    };
+
     return (
         <div>
+            {isLoading && <SpinnerOverlay />}
             <div className="search-section">
                 <input className="search-bar" type="text" value={searchTerm} onChange={handleSearchChange}></input>
                 <div className="filters-and-searchButton">
@@ -71,12 +104,13 @@ const Search = () => {
             </div>
             <div className="results-section">
                 <div className='cardSection-container'>
-                    <h1 className='topFive-header'>Results</h1>
+                    <h1 className='topFive-header'>Films</h1>
                     <div className='card-container'>
                         {filteredData.map((item, index) => (
-                            <Card key={index} id={index} title={item[0]} />
+                            <Card key={index} id={index} title={item[0]} openPopup={() => openPopup(item[0])} />
                         ))}
                     </div>
+                    <FilmPopup isActive={popupActive} closePopup={closeFilmPopup} details={filmDetails}/>
                 </div>
             </div>
         </div>
